c0ad01fc28a8695484f4184da9ccd6131a3cf495
User <gochazautashvili2017@gmail.com>
2024-01-17 14:09:08 +0400

    first commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use("/api", workoutsRoutes)
 app.use("/user", userRouter)
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 4000
 
 // API
 mongoose.connect(process.env.MONG_URL).then(() => {
